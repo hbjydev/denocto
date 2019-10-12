5585849ff4fe54463b56831c869e2815e0eb7791
Hayden Young <hayden@haydennyyy.com>
2019-10-12 23:27:55 +0100

    Include allow-net requirement error

--- mod.ts
+++ mod.ts
@@ -1,3 +1,9 @@
 // Copyright 2019 Hayden Young. MIT License.
+const { permissions } = Deno;
+
+if(!permissions().net) {
+  throw Error('You need to give this program `net` permissions to use Denocto.');
+}
+
 import * as user from './user.ts';
 export { user };
