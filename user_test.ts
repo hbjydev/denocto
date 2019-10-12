cc1be05d98d5361e91e9a3da791f2a2790bec3e4
Hayden Young <hayden@haydennyyy.com>
2019-10-13 00:02:39 +0100

    Look at @asottile's followers as a test

--- user_test.ts
+++ user_test.ts
@@ -3,9 +3,21 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
 import { getUser } from "./user.ts";
 
-test(async function userTest() {
-  const user = await getUser('asottile');
-  assertEquals(user.login, 'asottile');
-});
+test(
+  'user.login should be populated',
+  async function userTest() {
+    const user = await getUser('asottile');
+    assertEquals(user.login, 'asottile');
+  }
+);
+
+test(
+  'user.getFollowers should give a valid list of users',
+  async function userTest() {
+    const user = await getUser('asottile', '042b8a3cfb0d026bd872460625c678b98f3df4e3');
+    const followers = await user.getFollowers();
+    assertEquals(followers[0].id, 2161601);
+  }
+);
 
 runTests();
