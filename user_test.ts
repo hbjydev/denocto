ff5ac57abfcac21ad71b11d5db509247575839a4
Hayden Young <hayden@haydennyyy.com>
2019-10-12 23:24:31 +0100

    Fix an import

--- user_test.ts
+++ user_test.ts
@@ -1,7 +1,7 @@
 import { runTests, test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
-import { getUser } from "../user.ts";
+import { getUser } from "./user.ts";
 
 test(async function userTest() {
   const user = await getUser('haydennyyy');
