c747b1402d2810d841f1130c3c11ac5385dca4aa
Hayden Young <hayden@haydennyyy.com>
2019-10-13 03:25:31 +0100

    Include organization type

--- types.d.ts
+++ types.d.ts
@@ -164,3 +164,48 @@ export interface Event {
   id: string;
 }
 
+export interface Organization {
+  login: string;
+  id: number;
+  node_id: string;
+  url: string;
+  repos_url: string;
+  events_url: string;
+  hooks_url: string;
+  issues_url: string;
+  members_url: string;
+  public_members_url: string;
+  avatar_url: string;
+  description: string;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  email: string;
+  is_verified: boolean;
+  has_organization_projects: boolean;
+  has_repository_projects: boolean;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  html_url: string;
+  created_at: string;
+  type: string;
+  total_private_repos: number;
+  owned_private_repos: number;
+  private_gists: number;
+  disk_usage: number;
+  collaborators: number;
+  billing_email: string;
+  plan: {
+    name: string;
+    space: number;
+    private_repos: number;
+  };
+  default_repository_settings: string;
+  members_can_create_repositories: boolean;
+  two_factor_requirement_enabled: boolean;
+  members_allowed_repository_creation_type: string;
+}
+
